457769b294cf12e6b320b37b135fe725c09c5553
Andrey <andreich2010@gmail.com>
2024-09-30 14:26:10 +0300

    Tetris finish

--- app/src/main/java/com/example/tetrisnoactivity/MainActivity.kt
+++ app/src/main/java/com/example/tetrisnoactivity/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.tetrisnoactivity
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 import android.widget.Button
 import android.view.View
